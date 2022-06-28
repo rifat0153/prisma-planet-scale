69ce4956ae0d91f87fb01a4e94dc45805e3b6cec
Mahbubur Rahman <rifat0153@gmail.com>
2022-06-28 22:21:51 +0100

    Prisma Planet Scale Init

---
+++ src/post/post.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { PostService } from './post.service';
+import { Prisma } from '@prisma/client';
+
+@Controller('post')
+export class PostController {
+  constructor(private readonly postService: PostService) {}
+
+  @Post()
+  create(@Body() postCreateInput: Prisma.PostCreateInput) {
+    return this.postService.create(postCreateInput);
+  }
+
+  @Get()
+  findAll() {
+    return this.postService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.postService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() postUpdateInput: Prisma.PostUpdateInput,
+  ) {
+    return this.postService.update(+id, postUpdateInput);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.postService.remove(+id);
+  }
+}
---
+++ src/post/post.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from './../prisma.service';
+
+@Injectable()
+export class PostService {
+  constructor(private prisma: PrismaService) {}
+
+  create(postCreateInput: Prisma.PostCreateInput) {
+    return this.prisma.post.create({
+      data: postCreateInput,
+    });
+  }
+
+  findAll() {
+    return this.prisma.user.findMany({
+      include: {
+        posts: {
+          include: {
+            _count: {
+              select: { attachments: true },
+            },
+          },
+        },
+      },
+    });
+    return this.prisma.user.findMany({
+      include: {
+        _count: {
+          select: { posts: true },
+        },
+      },
+    });
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} post`;
+  }
+
+  update(id: number, updatePostDto: Prisma.PostUpdateInput) {
+    return `This action updates a #${id} post`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} post`;
+  }
+}
